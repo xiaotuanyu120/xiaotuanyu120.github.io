d2e0ee89d7658178858f135415c1227c83151caa
xiaotuanyu120 <zhaopeiwu@outlook.com>
2019-04-16 14:32:49 +0800

    reupload static

---
+++ docs/static/js/include-footer.js
@@ -0,0 +1,45 @@
+document.write('\
+  <footer>\
+    <div class="container">\
+      <div class="row footer-links">\
+        <div class="col-lg-2 col-sm-2">\
+          <h3>友情链接</h3>\
+          <ul>\
+            <li><a href="">标题1</a></li>\
+            <li><a href="">标题2</a></li>\
+          </ul>\
+        </div>\
+        <div class="col-lg-2 col-sm-2">\
+          <h3>footer 分类2</h3>\
+          <ul>\
+            <li><a href="">标题1</a></li>\
+            <li><a href="">标题2</a></li>\
+          </ul>\
+        </div>\
+        <div class="col-lg-2 col-sm-2">\
+          <h3>footer 分类3</h3>\
+          <ul>\
+            <li><a href="">标题1</a></li>\
+            <li><a href="">标题2</a></li>\
+          </ul>\
+        </div>\
+        <div class="col-lg-2 col-sm-2">\
+          <h3>footer 分类3</h3>\
+          <ul>\
+            <li><a href="">标题1</a></li>\
+            <li><a href="">标题2</a></li>\
+          </ul>\
+        </div>\
+        <div class="col-lg-4 col-sm-4">\
+          <h3>footer 长分类4</h3>\
+            <a class="social-icons footer-twitter" href="" target="_blank"></a>\
+            <a class="social-icons footer-facebook" href="" target="_blank"></a>\
+            <a class="social-icons footer-linkedin" href="" target="_blank"></a>\
+            <a class="social-icons footer-youtube" href="" target="_blank"></a>\
+            <p class="fine-print">\
+              网战由flask，nginx，uwsgi，mistune等技术支持。linux.xiao5tech.com由<a href="http://flask.pocoo.org/" target="_blank">flask</a>和<a href="https://nginx.org/" target="_blank">nginx</a>驱动。</p>\
+        </div>\
+      </div>\
+    </div>\
+  </footer>\
+');
